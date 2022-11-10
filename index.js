aad2d53265d4f818bbc716ef0881c494cd5af111
Bradley-Basik <114007065+Bradley-Basik@users.noreply.github.com>
2022-11-10 15:07:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
 */
+const path = require('path')
+const PORT = process.env.PORT || 5000
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
